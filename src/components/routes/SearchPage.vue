adb5161d94f3e8e758589c20e487df5301a5c8f8
SilentJungle399 <silentjungle399@gmail.com>
2021-03-24 18:19:20 +0530

    bare code for search results page

---
+++ src/components/routes/SearchPage.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="searcharea">
+		
+	</div>
+</template>
+
+<script>
+import progress from '@/handlers/progress.js';
+	
+export default {
+	name: "SearchPage",
+	props: {
+		searchres: {
+			required: true
+		}
+	},
+	mounted() {
+		progress.done()
+	}
+}
+</script>
+
+<style>
+	
+</style>
