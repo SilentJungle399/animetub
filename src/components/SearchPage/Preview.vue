7b7755b0c40ad5cd1618b4fbf4a585669693711d
SilentJungle399 <silentjungle399@gmail.com>
2021-03-24 18:19:40 +0530

    bare code for anime preview tile on search results page

---
+++ src/components/SearchPage/Preview.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="">
+		
+	</div>
+</template>
+
+<script>
+import progress from '@/handlers/progress.js';
+	
+export default {
+	name: "Preview",
+	props: {
+		searchres: {
+			required: true
+		}
+	},
+	mounted() {
+		progress.done()
+	}
+}
+</script>
+
+<style>
+	
+</style>
