364fdf68f06530ab46621c5442e08fe6cce7c86f
SilentJungle399 <silentjungle399@gmail.com>
2021-03-26 23:42:16 +0530

    smth which will grow into an anime page... no scraper for it rn so empty

---
+++ src/components/routes/Anime.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="animearea">
+		hi
+	</div>
+</template>
+
+<script>
+import progress from '@/handlers/progress.js';
+import recent from '@/handlers/recent.js';
+	
+export default {
+	name: "AnimePage",
+	mounted() {
+		progress.done()
+		const receps = recent.getRecent(this.$route.params.anime)
+		console.log(receps)
+	}
+}
+</script>
+
+<style>
+.animearea {
+	margin-top: 60px;
+}
+</style>
